7f6fc60e1eea8f4068c9fd8fc4d56c165745c939
Vinícius Melo <vinicius.simplicio@artit.com.br>
2024-09-01 18:31:10 -0300

    fix: code

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,7 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 export function GET(request: Request) {
-    fetch(`https://api.telegram.org/bot2127657888:AAFg-pNDY2OhgqQhsiVpfdiCK0SiIyBjLME/sendMessage?chat_id=97153814&text=Message 2 from ${process.env.VERCEL_REGION} => ${new Date().toISOString()}`)
+    fetch(`https://api.telegram.org/bot2055543371:AAEiICiuiYj9xcZ1hIoD-Jb06kGEUIf5sQQ/sendMessage?chat_id=97153814&text=${process.env.VERCEL_REGION} => ${new Date().toISOString()}`)
         .then((data) => console.log(data.json()));
 
     return new Response(`Hello from ${process.env.VERCEL_REGION}, ${new Date().toISOString()}`);
