999379c65ce504c984ef789721883a7556689b0a
Vinícius Melo <vinicius.simplicio@artit.com.br>
2024-09-01 15:15:08 -0300

    feat: cron jobs

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,7 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 export function GET(request: Request) {
+    fetch('https://api.telegram.org/bot2127657888:AAFg-pNDY2OhgqQhsiVpfdiCK0SiIyBjLME/sendMessage?chat_id=97153814&text=Hi+Everyone')
+    .then((data) => console.log(data.json()));
     return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
